9e46481fd5135bc4cb0be996cce45677f16009c9
serena-marie <serena.gutierrez0@gmail.com>
2019-06-02 16:44:38 -0500

    Updates

---
+++ precache-manifest.7a0ffda36c7c5091130f026408a863fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8df59d908ee058af2c7f89684bdaf131",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2eeebb19384c0f10bd6e",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "2eeebb19384c0f10bd6e",
+    "url": "/static/js/main.55e16174.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
